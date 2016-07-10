cff6890e38c1e3857e00e5d246d494872f2f6991
lucas <llzqingdao2012@gmail.com>
2016-07-10 14:23:46 +0800

    输入框layer加入圆角样式。

--- LemoniOS/Classes/Widgets/MessageComposerTextView.swift
+++ LemoniOS/Classes/Widgets/MessageComposerTextView.swift
@@ -8,7 +8,37 @@
 
 import UIKit
 
+struct MessageComposerTextViewUX {
+    
+    static let backgroundColor = UIColor.white()
+    static let borderWidth: CGFloat = 0.5
+    static let borderColor = UIColor.lightGray().cgColor
+    static let cornerRadius: CGFloat = 6.0
+}
+
 class MessageComposerTextView: UITextView {
     
+    override init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+        setupViews()
+    }
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupViews()
+    }
+}
+
+// MARK: - Private methods
+
+private extension MessageComposerTextView {
+    
+    private func setupViews() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.backgroundColor = MessageComposerTextViewUX.backgroundColor
+        self.layer.borderWidth = MessageComposerTextViewUX.borderWidth
+        self.layer.borderColor = MessageComposerTextViewUX.borderColor
+        self.layer.cornerRadius = MessageComposerTextViewUX.cornerRadius        
+    }
 }
